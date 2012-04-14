4e8f14a68eade229509319747efa7109fa2c6a3d
Michael Klein <mbklein@gmail.com>
2012-04-13 17:52:08 -0700

    Fix issue with auto-creation of Project on Release post

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,16 +7,14 @@ class ApplicationController < ActionController::Base
     base_key = key.to_s.sub(/_id$/,'').to_sym
     unless p.nil?
       val = params.delete(key) || params.delete(base_key) || p.delete(key) || p.delete(base_key)
-      Rails.logger.info key
-      Rails.logger.info val
       if val.is_a?(String) and val !~ /^[0-9]+$/
-        p[key] = klass.find_or_create_by_name(val).id
+        p[key] = klass.find_or_create_by_name(val) do |n|
+          n.update_attributes params[base_key]
+        end.id
       elsif val.is_a?(Fixnum) or val =~ /^[0-9]+$/
         p[key] = val.to_i
       end
     end
-    Rails.logger.info p.inspect
-    Rails.logger.info params.inspect
   end
   
   def resolve_params
