49adb8cca28125221c8423bccff0dfc68667a107
Michael Klein <mbklein@gmail.com>
2012-04-16 16:34:56 -0700

    Add test deployment config

---
+++ config/deploy/testing.rb
@@ -0,0 +1,9 @@
+set :rails_env, "test"
+set :deployment_host, "lyberapps-test.stanford.edu"
+set :repository,  "."
+set :branch, "master"
+set :bundle_without, [:deployment, :development]
+
+role :web, deployment_host
+role :app, deployment_host
+role :db,  deployment_host, :primary => true
