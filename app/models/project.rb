baa5327f6715ffa6e812fd79a3c075fec68d76d2
Michael Klein <mbklein@gmail.com>
2012-04-18 16:22:56 -0700

    Add default production release notification for all new projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,8 +4,18 @@ class Project < ActiveRecord::Base
   has_many :notifications, :dependent => :delete_all
   has_many :environments, :through => :releases, :select => 'distinct environments.*'
   validates :name, :presence => true, :uniqueness => true, :length => { :maximum => 20 }
+
+  after_create :add_default_notification
   
   def latest_releases
     releases.latest.also_index_by(:environment_name)
+  end
+  
+  def add_default_notification
+    env = Environment.find_by_name('production')
+    unless env.nil?
+      self.notifications.create :environment_id => env.id
+      self.notifications.reject! { |n| n.new_record? }
+    end
   end
 end
