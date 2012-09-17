4dc7abf366c1db1246891854486935f79b9ce520
Chris Beer <cabeer@stanford.edu>
2012-09-17 11:21:28 -0700

    remove basic auth credentials

--- config/initializers/basic_auth.rb
+++
@@ -1,6 +0,0 @@
-auth_file = File.join(Rails.root, 'config/auth.yml')
-if File.exists?(auth_file)
-  ApplicationController.credentials = YAML.load(File.read(auth_file))
-else
-  Rails.logger.warn "#{auth_file} not found. Create/Update/Destroy requests will not be protected!"
-end
