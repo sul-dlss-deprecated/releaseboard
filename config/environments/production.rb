ee3cc7660f12895b03000197f571c78ccd24c569
Chris Beer <cabeer@stanford.edu>
2012-09-21 09:52:00 -0700

    set timezone to PT

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,4 +51,6 @@ Releaseboard::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  config.time_zone = 'Pacific Time (US & Canada)'
 end
